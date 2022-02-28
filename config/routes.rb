1410cad3517abee3cb40fbbe49039bd76c226af0
ting.chen <ting.chen@nextlink.com.tw>
2022-02-28 19:18:49 +0800

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :members, except: :show
+  resources :members, except: :show do
+    resources :attachment, only: %i[new create] do
+      member do
+        post :download
+      end
+    end
+  end
 
   root to: 'members#index'
 end
