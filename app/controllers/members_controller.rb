b287f2e9f061111636a08730adbaf4d94cdf1e55
ting.chen <ting.chen@nextlink.com.tw>
2022-02-28 18:44:29 +0800

    new && create done

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,4 +2,24 @@ class MembersController < ApplicationController
   def index
     @members = Member.all
   end
+
+  def new
+    @member = Member.new
+  end
+
+  def create
+    @member = Member.new(member_params)
+
+    if @member.save
+      redirect_to members_path, notice: '新增 Member 成功!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name)
+  end
 end
