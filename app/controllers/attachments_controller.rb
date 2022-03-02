87bf239edf3b03b792b2b03c69da0b40839e7547
ting.chen <ting.chen@nextlink.com.tw>
2022-03-02 12:18:02 +0800

    上傳檔案

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,3 +1,5 @@
+require "google/cloud/storage"
+
 class AttachmentsController < ApplicationController
   before_action :find_member, only: %i[new create]
 
@@ -8,10 +10,24 @@ class AttachmentsController < ApplicationController
   def create
     return redirect_to request.referer if params[:upload_file].blank?
 
-    if @member.attachments.create(file_name: file_name)
+    attachment = @member.attachments.new(
+      file_name: file_name,
+      storage_path: "upload_file/#{@member.name}/#{Time.current.to_i}_#{file_name}",
+    )
+
+    return render :new unless attachment.valid?
+
+    storage = Google::Cloud::Storage.new(
+      project_id: ENV['GOOGLE_CLOUD_PROJECT'],
+      credentials: ENV['GOOGLE_CLOUD_CREDENTIALS']
+    )
+    bucket = storage.bucket ENV['MY_BUCKET']
+    upload_result = bucket.create_file(params[:upload_file].path, attachment.storage_path)
+
+    if upload_result && attachment.save!
       redirect_to members_path, notice: "已成功上傳 #{file_name}"
     else
-      render :new
+      redirect_to members_path, notice: "上傳 #{file_name} 失敗，請再次上傳"
     end
   end
 
