c196961c188db38a4dfe28fcef0317a56dc65d38
ting.chen <ting.chen@nextlink.com.tw>
2022-05-21 14:09:22 +0800

    修改 model

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,7 @@
 class Member < ApplicationRecord
   has_many :attachments, dependent: :destroy
 
-  validates :name, presence: { message: '請填入名稱' }
+  validates 'name_zh-TW', presence: { message: '請填入名稱' }
+  validates 'name_en', presence: { message: 'please fill in name' }
+
 end
