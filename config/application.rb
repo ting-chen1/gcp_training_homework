cace4dc429869cea3c08a55c04c08a93a033d71d
ting.chen <ting.chen@nextlink.com.tw>
2022-05-21 14:10:45 +0800

    I18n 設定與翻譯檔案

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module GcpTrainingHomework
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # i18n 設定
+    config.i18n.fallbacks = [I18n.default_locale]
+    config.i18n.default_locale = :"zh-TW"
+    config.i18n.available_locales = %i[zh-TW en]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
