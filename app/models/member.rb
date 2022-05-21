3e5ce19ae2145246a36950a0393c73b70126a590
ting.chen <ting.chen@nextlink.com.tw>
2022-05-21 15:48:29 +0800

    member 建立會給 slug

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,4 +4,11 @@ class Member < ApplicationRecord
   validates 'name_zh-TW', presence: { message: '請填入名稱' }
   validates 'name_en', presence: { message: 'please fill in name' }
 
+  before_create :set_slug
+
+  private
+
+  def set_slug
+    self.slug = Digest::SHA2.hexdigest(name_en).first(10)
+  end
 end
